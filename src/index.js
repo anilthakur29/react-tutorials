43d9b1d3950bd0607b5aac7e96e053a0ca36ade1
anil <anilthakur2988@gmail.com>
2019-05-19 22:56:38 +0530

    clock app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+setInterval(function(){
 ReactDOM.render(
-	<h1>Welcome Anil Thankur</h1>,
+	<h1>Time Now-{new Date().toLocaleTimeString()}</h1>,
 	document.getElementById('root')
 	);
+},1000);
